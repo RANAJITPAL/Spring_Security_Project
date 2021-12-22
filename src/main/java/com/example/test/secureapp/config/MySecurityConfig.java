6822a66d157e8ee04e8b678f56660bcf898b2a07
RANAJITPAL <palranajit98@gmail.com>
2021-12-22 10:53:42 +0530

    use custom filter

--- src/main/java/com/example/test/secureapp/config/MySecurityConfig.java
+++ src/main/java/com/example/test/secureapp/config/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.test.secureapp.config;
 
 import com.example.test.secureapp.utility.MyAuthenticationProvider;
+import com.example.test.secureapp.utility.MySecurityFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
@@ -36,9 +38,10 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-//        http.httpBasic();
-        http.formLogin();
-        http.authorizeRequests().anyRequest().authenticated();
+        http.httpBasic();
+//        http.formLogin();
+        http.authorizeRequests().antMatchers("/hello").authenticated();
+        http.addFilterBefore(new MySecurityFilter(), BasicAuthenticationFilter.class);
     }
 
     @Bean
--- src/main/java/com/example/test/secureapp/controller/HelloController.java
+++ src/main/java/com/example/test/secureapp/controller/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String hello(){
         return "Hello Spring Security";
     }
+
+    @GetMapping("/bye")
+    public String bye(){
+        return "bye Spring Security";
+    }
 }
